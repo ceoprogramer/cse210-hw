7973418bfa0e0951dfa4dc92df9e4e53182c433f
ceoprogramer <esanchezsolano@byupathway.edu>
2026-01-08 15:20:18 -0600

    update exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Guess the magic number (between 1 and 100): ");
+            
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You've guessed the magic number!");
+            }
+        }
     }
 }
