2162ea27dd96716406f6b400b214ba633671738c
ceoprogramer <esanchezsolano@byupathway.edu>
2026-01-08 19:13:15 -0600

    update exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -7,3 +7,29 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
     }
 }
+
+static void DisplayWelcomeMessage()
+{
+    Console.WriteLine("Welcome to the Program!");
+}
+
+static void PromptUserName()
+{
+    Console.Write("Please enter your name: ");
+    string name = Console.ReadLine();
+    return name;
+}
+
+static int PromptUserNumber()
+{
+    Console.Write("Please enter your favorite number: ");
+    string input = Console.ReadLine();
+    int number = int.Parse(input);
+    return number;
+}
+
+static int SquareNumber(int number)
+{
+    int square = number * number;
+    return square;
+}
