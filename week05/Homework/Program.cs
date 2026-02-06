62f697353d7eb755688a3425c62c6d68b0686bd5
ceoprogramer <esanchezsolano@byupathway.edu>
2026-02-05 21:27:34 -0600

    Update Program

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Sara Bush", "History");
+        Console.WriteLine(a1.GetSummary());
+
+        // 
+        MathAssignment a2 = new MathAssignment("Ana Smith", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Lea Jones", "Big leaders", "Economics in the world");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
